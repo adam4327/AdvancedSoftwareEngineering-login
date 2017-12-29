e0b442eca0f0992d42ce0ce46e9b942b8629bbc5
adam4327 <adam.davies1996@hotmail.co.uk>
2017-12-29 17:26:41 +0000

    first commit

---
+++ login/login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace login
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        //EXIT BUTTON
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //LOGIN BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
